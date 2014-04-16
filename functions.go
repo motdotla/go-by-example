61503ac4beaba10ffeae78d9cc8964d804e2b3ec
scottmotte <scott@scottmotte.com>
2014-04-16 09:34:56 -0700

    minor

--- functions.go
+++ functions.go
@@ -4,33 +4,33 @@ import "fmt"
 
 //funcs
 func plus(a int, b int) int {
-  return a + b
+	return a + b
 }
 
 //multiple return values
 func vals() (int, int) {
-  return 88, 77
+	return 88, 77
 }
 
 //Varidatic function
 func sum(nums ...int) int {
-  total := 0
-  for i := 0; i < len(nums); i++ {
-    total = total + nums[i]
-  }
+	total := 0
+	for i := 0; i < len(nums); i++ {
+		total = total + nums[i]
+	}
 
-  return total
+	return total
 }
 
 func main() {
-  result := plus(9,3)
-  fmt.Println(result)
+	result := plus(9, 3)
+	fmt.Println(result)
 
-  a, b := vals()
+	a, b := vals()
 
-  fmt.Println(a)
-  fmt.Println(b)
+	fmt.Println(a)
+	fmt.Println(b)
 
-  total := sum(vals())
-  fmt.Println(total)
+	total := sum(vals())
+	fmt.Println(total)
 }
